b402d60857e5b5ec645c3d73435c16829dff2a1d
qn19325 <qn19325@bristol.ac.uk>
2021-04-19 18:41:45 +0100

    Think its finished

--- coursework/datafiles/lsr.py
+++ coursework/datafiles/lsr.py
@@ -9,21 +9,27 @@ def load_points_from_file(filename):
     return points[0].values, points[1].values
     
 def view_data_segments(xs, ys, a):
-    print(a)
     assert len(xs) == len(ys)
     assert len(xs) % 20 == 0
     len_data = len(xs)
     num_segments = len_data // 20
     colour = np.concatenate([[i] * 20 for i in range(num_segments)])
     plt.set_cmap('Dark2')
-    x = np.linspace(0,30)
-    if len(a) == 2:
-        y = a[1] * x + a[0]
-    elif len(a) == 4:
-        y = a[3] * x**3 + a[2] * x**2 +  a[1] * x + a[0]
+    _, x_segment, y_segment = split_into_segment(xs, ys)
     fig, ax = plt.subplots()
-    ax.scatter(xs, ys, c=colour)
-    plt.plot(x, y)
+    ax.scatter(x_segment, y_segment, c=colour)
+    for idx, val in enumerate(x_segment):
+        x = np.linspace(min(x_segment[idx]),max(x_segment[idx]))
+        coefficients = a[idx]
+        if len(coefficients) == 2:
+            y = coefficients[1] * x + coefficients[0]
+            plt.plot(x, y)
+        elif len(coefficients) == 3:
+            y = coefficients[2] * np.sin(x) + coefficients[1] * np.cos(x) + coefficients[0]
+            plt.plot(x, y)
+        else:
+            y = coefficients[3] * x**3 + coefficients[2] * x**2 + coefficients[1] * x + coefficients[0]
+            plt.plot(x,y)
     plt.show()
 
 def split_into_segment(xs, ys):
@@ -45,11 +51,21 @@ def non_linear_least_squares(xs, ys):
     A = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(ys)
     return A
 
+def unknown_func_least_squares(xs, ys):
+    X = np.column_stack((np.ones(xs.shape), np.cos(xs), np.sin(xs)))
+    A = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(ys)
+    return A
+
 def sum_squared_error(coefficients, xs, ys):
     sum_squared_error = 0
     if len(coefficients) == 2:
         for idx, val in enumerate(xs):
             fitted_y = coefficients[1] * val + coefficients[0]
+            actual_y = ys[idx]
+            sum_squared_error = sum_squared_error + ((actual_y - fitted_y)**2)
+    elif len(coefficients) == 3:
+        for idx, val in enumerate(xs):
+            fitted_y = y = coefficients[2] * np.sin(val) + coefficients[1] * np.cos(val) + coefficients[0]
             actual_y = ys[idx]
             sum_squared_error = sum_squared_error + ((actual_y - fitted_y)**2)
     elif len(coefficients) == 4:
@@ -59,21 +75,77 @@ def sum_squared_error(coefficients, xs, ys):
             sum_squared_error = sum_squared_error + ((actual_y - fitted_y)**2)
     return sum_squared_error
 
+def cross_validation(xs, ys, k):
+    data = np.column_stack((xs, ys))
+    np.random.shuffle(data)
+    avg = len(data) / float(k)
+    split_data = []
+    last = 0.0
+
+    while last < len(data):
+        split_data.append(data[int(last):int(last + avg)])
+        last += avg
+    linear_error = []
+    non_linear_error = []
+    unknown_error = []
+    for i in range(len(split_data)):
+        test = split_data[i]
+        training = []
+        for j in range(len(split_data)):
+            if i != j:
+                training.extend(split_data[j])
+        training = np.vstack(training)
+
+        linear_ls = linear_least_squares(training[:,0], training[:,1])
+        non_linear_ls = non_linear_least_squares(training[:,0], training[:,1])
+        unknown_ls = unknown_func_least_squares(training[:,0], training[:,1])
+
+        linear_sse = sum_squared_error(linear_ls, test[:,0], test[:,1])
+        non_linear_sse = sum_squared_error(non_linear_ls, test[:,0], test[:,1])
+        unknown_sse = sum_squared_error(unknown_ls, test[:,0], test[:,1])
+
+        linear_error.append(linear_sse)
+        non_linear_error.append(non_linear_sse)
+        unknown_error.append(unknown_sse)
+
+    return linear_error, non_linear_error, unknown_error
+    
+
 
 
 filename = sys.argv[1]
 x, y = load_points_from_file(filename)
 num_segments, xs, ys = split_into_segment(x, y)
-A = linear_least_squares(x, y)
-B = non_linear_least_squares(x, y)
-print(A,B)
-linear_sse = sum_squared_error(A, x, y)
-non_linear_sse = sum_squared_error(B, x, y)
-if(linear_sse < non_linear_sse):
-    view_data_segments(x, y, A)
-else:
-    view_data_segments(x, y, B)
+coefficients = []
+for i in range(num_segments):
+    linear_ls = linear_least_squares(xs[i], ys[i])
+    non_linear_ls = non_linear_least_squares(xs[i], ys[i])
+    unknown_ls = unknown_func_least_squares(xs[i], ys[i])
 
-# e, f = linear_least_squares(xs[0], ys[0])
-# a, b, c, d = non_linear_least_squares(xs[2], ys[2])
-# view_data_segments(x,y, a, b, c, d, e, f)
+    # linear_sse = sum_squared_error(linear_ls, xs[i], ys[i])
+    # non_linear_sse = sum_squared_error(non_linear_ls, xs[i], ys[i])
+    # unknown_sse = sum_squared_error(unknown_ls, xs[i], ys[i])
+
+    linear_errors, non_linear_errors, unknown_errors = cross_validation(xs[i], ys[i], 3)
+
+    print(linear_errors)
+    print(non_linear_errors)
+    print(unknown_errors)
+
+    linear_sse = np.mean(linear_errors)
+    non_linear_sse = np.mean(non_linear_errors)
+    unknown_sse = np.mean(unknown_errors)
+
+    print(linear_sse)
+    print(non_linear_sse)
+    print(unknown_sse)
+    
+    if(linear_sse < non_linear_sse and linear_sse < unknown_sse):
+        coefficients.append(linear_ls)
+    elif(non_linear_sse < linear_sse and non_linear_sse < unknown_sse):
+        coefficients.append(non_linear_ls)
+    else:
+        coefficients.append(unknown_ls)
+
+
+view_data_segments(x, y, coefficients)
